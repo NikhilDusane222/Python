70a5ab2c28867a15bcc1c6ab14fa61524ba957c5
Nikhil <you@example.com>
2020-04-01 17:40:21 +0530

    Added: Calculate the windchill

---
+++ FunctionalPrograms/windChill.py
@@ -0,0 +1,15 @@
+import math
+
+#input temperature
+temp=int(input("Temperature is: "))
+if temp>=0 and temp<=50:
+    #input wind speed
+    speed = int(input("Wind speed is: "))
+    if speed>3 and speed<120:
+        #calculate windchill
+        wind = 35.74 + 0.6215 * temp + (0.4275 * temp - 35.75) * (math.pow(speed, 0.16))
+        print("Wind chill is: ",wind)
+    else:
+        print("Enter correct values!")
+else:
+    print("Enter correct values!")
